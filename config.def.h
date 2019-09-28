286bc541471e96279a7dcaa631a5c341b10caf1e
Vlad Tutunea <vladovidiu.tutunea@gmail.com>
2019-09-28 19:58:35 +0100

    font-patch

--- config.def.h
+++ config.def.h
@@ -5,9 +5,9 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mono:pixelsize=16:antialias=true:autohint=true";
-static char *font2[] = { "Inconsolata for Powerline:pixelsize=16:antialias=true:autohint=true" };
-static int borderpx = 2;
+static char *font = "mono:pixelsize=14:antialias=true:autohint=true";
+static char *font2[] = { "Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true" };
+static int borderpx = 1;
 
 /*
  * What program is execed by st depends of these precedence rules:
@@ -84,7 +84,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.92;
+float alpha = 0.60;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
